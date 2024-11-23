e54a2b4a4f537ac84c1054a7de442c136cf30a92
IgorNara <igornaravieira@gmail.com>
2024-11-23 15:27:54 -0300

    adiciona enum tamanho

---
+++ desafio-back/api/src/model/tamanho/CampoUnicoTamanho.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+enum CampoUnicoTamanho: string {
+    case PP = "PP";
+    case P = "P";
+    case M = "M";
+    case G = "G";
+    case GG = "GG";
+    case XG = "XG";
+}
+
+?>
