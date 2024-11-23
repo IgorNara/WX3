ae964004196f14eb5b1d290508c210ee6c35dfcf
IgorNara <igornaravieira@gmail.com>
2024-11-23 15:32:45 -0300

    adiciona htaccess e manipulação de rotas no index

---
+++ desafio-back/api/index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/src/model/funcoes-util.php';
+
+$dados = json_decode( file_get_contents( "php://input" ), true ) ?? [];
+
+$logica = obterLogica();
+$conexao = getConexao();
+
+$rota = new Rota( $_SERVER, $dados );
+$arrayRotas = require_once __DIR__ . "/src/rotas.php";
+
+if( $logica === "/cliente/logar" ) {
+    $rotas = require_once __DIR__ . "/controller/rotas-cliente.php";
+    $rota->executarRota( $rotas );
+}
+else {
+    require_once __DIR__ . '/src/model/autenticar.php';
+    foreach( $arrayRotas as $rotas ) {
+        if( isset( $rotas[$logica] ) ) 
+            $rota->executarRota( $rotas );
+    }
+}
+?>
---
+++ desafio-back/api/src/rotas.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    require_once __DIR__ . "/controller/rotas-categoria.php",
+    require_once __DIR__ . "/controller/rotas-cliente.php",
+    require_once __DIR__ . "/controller/rotas-endereco.php",
+    require_once __DIR__ . "/controller/rotas-pedido.php",
+    require_once __DIR__ . "/controller/rotas-produto.php",
+    require_once __DIR__ . "/controller/rotas-tamanho.php",
+    require_once __DIR__ . "/controller/rotas-venda.php"
+]
+
+?>
